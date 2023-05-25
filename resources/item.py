77a29c8a84e5b0ba1e4529b497912997ecb429a0
Bung Danar <danar.g.putera@gmail.com>
2023-05-25 11:49:37 +0700

    chore: Use model to insert data

--- resources/item.py
+++ resources/item.py
@@ -1,10 +1,13 @@
-from uuid import uuid4
+# from uuid import uuid4
 
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
 
 from common.schemas import ItemSchema, ItemUpdateSchema
+from common.db import db
+from models import ItemModel
 
 blp = Blueprint("items", __name__, description="Operation on items")
 
@@ -55,23 +58,40 @@ class ItemList(MethodView):
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
+        '''
+        Using default request object from flask
+        '''
         # item_data = request.get_json()
 
         # if ("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
         #     abort(400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload")
 
-        for item in items.values():
-            if (item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]):
-                abort(400, message="Item already exists")
+        '''
+        Using marshmallow object
+        '''
+        # for item in items.values():
+        #     if (item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]):
+        #         abort(400, message="Item already exists")
 
-        if item_data["store_id"] not in stores:
-            abort(404, message="Store not found")
+        # if item_data["store_id"] not in stores:
+        #     abort(404, message="Store not found")
 
-        item_id = uuid4().hex
-        item = {
-            **item_data,
-            "id": item_id
-        }
-        items[item_id] = item
+        # item_id = uuid4().hex
+        # item = {
+        #     **item_data,
+        #     "id": item_id
+        # }
+        # items[item_id] = item
+
+        '''
+        Using sql alchemy model
+        '''
+        item = ItemModel(**item_data)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
 
         return item, 201
--- resources/store.py
+++ resources/store.py
@@ -1,10 +1,13 @@
-from uuid import uuid4
+# from uuid import uuid4
 
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 from common.schemas import StoreSchema
+from common.db import db
+from models import StoreModel
 
 blp = Blueprint("stores", __name__, description="Operation on stores")
 
@@ -39,22 +42,41 @@ class StoreList(MethodView):
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
     def post(self, store_data):
+        '''
+        Using default request object from flask
+        '''
         # store_data = request.get_json()
 
         # if "name" not in store_data:
         #     abort(
         #         400, message="Bad request. Ensure 'name' is included in the JSON payload.")
 
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message="Store already exists.")
+        '''
+        Using marshmallow object
+        '''
+        # for store in stores.values():
+        #     if store_data["name"] == store["name"]:
+        #         abort(400, message="Store already exists.")
 
-        store_id = uuid4().hex
+        # store_id = uuid4().hex
 
-        store = {
-            **store_data,
-            "id": store_id
-        }
+        # store = {
+        #     **store_data,
+        #     "id": store_id
+        # }
 
-        stores[store_id] = store
+        # stores[store_id] = store
+
+        '''
+        Using sql alchemy model
+        '''
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store with that name already exists.")
+        except SQLAlchemyError:
+            abort(500, message="An error occurred when creating store.")
+
         return store, 201
